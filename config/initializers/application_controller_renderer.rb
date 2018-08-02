3378252666ec97b12bb5decd25b409b2243c0ab0
fuchida64 <crbsfy731@gmail..com>
2018-08-02 11:24:36 +0000

    create show_edit_update for_user

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '189bc9796dec9694460a965638ef41ca7c2ca34ff11876dd43176da385af41a8837256ed4f2dd7619b62b66f4ca4f926bb0c1673b59e489d000bc6b57c0a6815'
