dfc8df027b7bfad36416fc4371049661116a55d7
fuchida64 <crbsfy731@gmail..com>
2018-08-05 03:36:19 +0000

    create edit to password

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( task_group.js )
+
+Rails.application.config.assets.precompile += %w( task_groups/index.scss )
+Rails.application.config.assets.precompile += %w( task_groups/show.scss )
