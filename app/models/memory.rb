f0d5ea0f56f7d5ce331861f8dcac27ee8c4cd046
fuchida64 <crbsfy731@gmail..com>
2018-09-09 12:58:00 +0000

    finish show in memory

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -5,11 +5,19 @@ class Memory < ApplicationRecord
 	attachment :problem_image
 	attachment :answer_image
 
+	validates :image_or_conetnt, presence: true
+
 	# before_create do
 	# 	problem.mark_for_destruction if problem.problem_content.blank?
 	# 	problem_image.mark_for_destruction if problem_image.problem_image.blank?
 	# 	answer.mark_for_destruction if answer.answer_content.blank?
 	# 	answer_image.mark_for_destruction if answer_image.answer_image.blank?
 	# end
+
+	private
+
+    def image_or_conetnt
+		problem_image.presence or problem_content.presence
+    end
 
 end
